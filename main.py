9d9caad1b90816f8ee66c7973c53867fe29313ae
Mert Kara <mert.kara@appdirect.com>
2018-01-04 12:48:05 -0500

    update the routing to fix mismatching

--- main.py
+++ main.py
@@ -15,7 +15,7 @@
 import webapp2
 
 from api_handlers import AccountApi, ChatroomApi, UserAccessApi, AllUsersInChatroom, ChatroomPostApi, AccountDomainOwnershipApi, \
-    OwnershipVerificationApi, AccountDomainApi
+    OwnershipVerificationApi, AccountDomainApi, AccountChatroomApi
 from error_handlers import handle_400, handle_404, handle_409, handle_500
 from html_handlers import RedirectToAccounts, AllAccounts, SingleAccount, SingleRoom, AllRooms
 
@@ -27,14 +27,14 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/accounts/<account_id>/domains/<domain_name>', AccountDomainApi),
     webapp2.Route('/accounts/<account_id>/domains/<domain_name>/ownershipProof', AccountDomainOwnershipApi),
     webapp2.Route('/accounts/<account_id>/domains/<domain_name>/triggerOwnershipVerification', OwnershipVerificationApi),
-    webapp2.Route('/accounts/<account_id>/rooms', ChatroomApi),
+    webapp2.Route('/accounts/<account_id>/rooms', AccountChatroomApi),
     webapp2.Route('/rooms', ChatroomApi, methods=["GET"], handler_method="get_all_rooms"),
     webapp2.Route('/rooms/view', SingleRoom),
     webapp2.Route('/rooms/list', AllRooms),
     webapp2.Route('/rooms/<chatroom_id>/users', AllUsersInChatroom),
     webapp2.Route('/rooms/<chatroom_id>/users/<email>', UserAccessApi),
     webapp2.Route('/rooms/<chatroom_id>/posts', ChatroomPostApi),
-    webapp2.Route('/rooms/<chatroom_id>', ChatroomApi, methods=["GET"], handler_method="get_room")
+    webapp2.Route('/rooms/<chatroom_id>', ChatroomApi)
 ], debug=True)
 app.error_handlers[400] = handle_400
 app.error_handlers[404] = handle_404
